8a325c6aa3790c5c8cd6a013ac1de2c438c93f2b
Aicha-ch <chwikhi.aycha@gmail.com>
2025-03-02 20:44:40 +0100

    determine if all boxes can be opened

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Lockboxes
+    """
+    if not boxes:
+        return False
+
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    keys = [0]
+
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                keys.append(key)
+
+    return all(unlocked)
