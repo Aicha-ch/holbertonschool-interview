02651cf8e1c80a55a89d3da04a8cae3eb22ed506
Aicha-ch <chwikhi.aycha@gmail.com>
2025-07-13 20:10:37 +0200

    update

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -1,51 +1,87 @@
 #include "sort.h"
-
 /**
- * radix_sort - sorts by RADIX
- * @A: array to sort
- * @size: size of array
+ * freegrid - frees a matrix
+ * @grid: double poiter to free
+ * Return: No return
  */
-void radix_sort(int *A, size_t size)
+void freegrid(int **grid)
 {
-	size_t i;
-	int max = INT_MIN, *B;
-	long exp;
+	int i;
 
-	if (!A || size < 2)
-		return;
-	B = malloc(sizeof(*B) * size);
-	if (!B)
-		return;
-	for (i = 0; i < size; i++)
-		max = A[i] > max ? A[i] : max;
-	for (exp = 1; max / exp > 0; exp *= RADIX)
-	{
-		count_sort(A, size, B, exp);
-		print_array(A, size);
-	}
-	free(B);
+	for (i = 0; i < 10; i++)
+		free(grid[i]);
+	free(grid);
 }
-
 /**
- * count_sort - sort by current digit
- * @A: array to sort
- * @size: size of array
- * @B: malloced temp array
- * @exp: current digital exponent
- * Return: if array changed
+ * power_ten- calculates power of 10
+ * @power: power to calculate
+ * Return: 10**power
  */
-int count_sort(int *A, ssize_t size, int *B, long exp)
+int power_ten(int power)
 {
-	ssize_t i;
-	int count[RADIX] = {0}, ret = 0;
+	int i, result = 1;
+
+	for (i = 0; i < power; i++)
+		result *= 10;
+	return (result);
+}
+/**
+ * sort_in_matrix - sort by one digit in a matrix
+ * @array: array to sort
+ * @tmp: matrix to put the sorted numbers
+ * @size: size of arrray
+ * @level: digit to apply sortting
+ * Return: 0 if the digit is the mostright digit, otherwise 0
+ */
+int sort_in_matrix(int *array, int **tmp, size_t size, int level)
+{
+	int n, index[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, k, max = 0;
+	size_t i, j;
 
 	for (i = 0; i < size; i++)
-		count[(A[i] / exp) % RADIX]++, B[i] = 0;
-	for (i = 1; i < RADIX; i++)
-		count[i] += count[i - 1];
-	for (i = size - 1; i >= 0; i--)
-		B[--count[(A[i] / exp) % RADIX]] = A[i];
-	for (i = 0; i < size; i++)
-		A[i] = B[i];
-	return (ret);
+	{
+		n = (array[i] % power_ten(level)) / power_ten(level - 1);
+		tmp[n][index[n]++] = array[i];
+		if (array[i] > max)
+			max = array[i];
+	}
+	k = 0;
+	j = 0;
+	for (i = 0; i < 10; i++)
+	{
+		while (index[i] != 0 && k < index[i])
+		{
+			array[j + k] = tmp[i][k];
+			k++;
+		}
+		j += k;
+		k = 0;
+	}
+	print_array(array, size);
+
+	if (max / power_ten(level) == 0)
+		return (0);
+	return (1);
+
+}
+/**
+ * radix_sort - apply radix sort to array
+ * @array: array to sort
+ * @size: size of the array
+ *
+ * Return: No return
+ */
+void radix_sort(int *array, size_t size)
+{
+	int **tmp, flag = 1, level = 1;
+	size_t i;
+
+	if (!array || size < 2)
+		return;
+	tmp = malloc(sizeof(int *) * 10);
+	for (i = 0; i < 10; i++)
+		tmp[i] = malloc(sizeof(int) * size);
+	while (flag)
+		flag = sort_in_matrix(array, tmp, size, level++);
+	freegrid(tmp);
 }
